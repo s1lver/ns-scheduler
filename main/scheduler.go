02af322e7fd971233b528a742b118d44cfe13175
Moiseenko <s1lverhron@gmail.com>
2023-08-05 12:52:55 +0400

    Added definition day of week

--- main/scheduler.go
+++ main/scheduler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	schedulerConfig "ns-scheduler/config"
+	"time"
 )
 
 func main() {
@@ -14,9 +15,17 @@ func main() {
 	// Accessing the data from the YAML structure
 	standardSchedule := config.Schedules["standard"]
 
+	now := time.Now()
+	date := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+	dayOfWeek := int(date.Weekday())
+
 	for _, weekday := range standardSchedule.Weekdays {
-		fmt.Printf("Days: %v\n", weekday.Days)
-		fmt.Printf("Start: %s\n", weekday.Start)
-		fmt.Printf("Stop: %s\n", weekday.Stop)
+		for days := range weekday.Days {
+			if dayOfWeek == days {
+				fmt.Printf("Days: %v\n", weekday.Days)
+				fmt.Printf("Start: %s\n", weekday.Start)
+				fmt.Printf("Stop: %s\n", weekday.Stop)
+			}
+		}
 	}
 }
